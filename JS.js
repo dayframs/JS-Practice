4b6b56f90314e9892a6605a50d2ce60765a679a0
dayframs <dayframs09@yahoo.com>
2023-08-26 11:20:48 +0100

    parameter functions removed

--- JS.js
+++ JS.js
@@ -1,13 +1,70 @@
 function multiply() {
     myResult.innerText = inp1.value * inp2.value;
 }
-function display(y) {
-    displayResult.innerText += y;
+
+
+// function display(y) {
+//     displayResult.innerText += y;
+// }
+// function calculate() {
+//     let z = displayResult.innerText;
+//     displayResult.innerText = eval(z);
+// }
+// function clearDisplay() {
+//     displayResult.innerText = "";
+// }
+
+
+// Calculator Java Script
+ function display1() {
+     result.innerText += '1';
+ }
+ function display2() {
+     result.innerText += '2';
 }
-function calculate() {
-    let z = displayResult.innerText;
-    displayResult.innerText = eval(z);
+function display3() {
+     result.innerText += '3';
 }
-function clearDisplay() {
-    displayResult.innerText = "";
+function display4() {
+     result.innerText += '4';
+}
+function display5() {
+     result.innerText += '5';
+}
+function display6() {
+     result.innerText += '6';
+}
+function display7() {
+    result.innerText += '7';
+}
+function display8() {
+     result.innerText += '8';
+}
+function display9() {
+     result.innerText += '9';
+}
+function display0() {
+     result.innerText += '0';
+}
+function displayDot() {
+     result.innerText += '.';
+}
+function displayA() {
+     result.innerText += '+';
+}
+function displayS() {
+     result.innerText += '-';
+}
+function displayD() {
+     result.innerText += '/';
+}
+function displayM() {
+     result.innerText += '*';
+}
+function calculateA() {
+    let z = result.innerText;
+    result.innerText = eval(z);
+}
+function clearA() {
+     result.innerText = '';
 }
